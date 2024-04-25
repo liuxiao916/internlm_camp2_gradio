6076393402f6e80250b490bb9bc008034dbe364c
Xiao Liu 20.04 <971177213@qq.com>
2024-04-25 18:46:01 +0800

    without half

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ os.system(f'git clone https://code.openxlab.org.cn/liuxiao916/personal_assistant
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True)
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
